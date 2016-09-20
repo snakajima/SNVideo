aa53232eaff2f900a3d809c686a59f6ef05f8537
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2016-09-20 16:30:42 -0700

    capture session

--- NSVideo/NSVideo/ViewController.swift
+++ NSVideo/NSVideo/ViewController.swift
@@ -7,12 +7,71 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet var viewMain : UIView!
+    var playerLayer : CALayer?
+
+    let session = AVCaptureSession()
+    var backCamera : AVCaptureDevice? = {
+        for device in AVCaptureDevice.devices() as! [AVCaptureDevice] {
+            if device.hasMediaType(AVMediaTypeVideo) {
+                if device.position == AVCaptureDevicePosition.Back {
+                    return device
+                }
+            }
+        }
+        return nil
+    }()
+    lazy var cameraInput:AVCaptureDeviceInput? = {
+        return try? AVCaptureDeviceInput(device: self.backCamera)
+    }()
+    lazy var imageOutput:AVCaptureStillImageOutput? = {
+        let output = AVCaptureStillImageOutput()
+        output.outputSettings = [ AVVideoCodecKey : AVVideoCodecJPEG ]
+        return output
+    }()
+    lazy var videoConnection:AVCaptureConnection? = {
+        for connection in self.imageOutput!.connections as! [AVCaptureConnection] {
+            for port in connection.inputPorts {
+                if port.mediaType == AVMediaTypeVideo {
+                    return connection
+                }
+            }
+        }
+        return nil
+    }()
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        playerLayer?.frame = viewMain.bounds
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if session.canSetSessionPreset(AVCaptureSessionPreset1280x720) {
+            session.sessionPreset = AVCaptureSessionPreset1280x720
+        }
+
+        if let input = cameraInput {
+            if session.canAddInput(input) {
+                session.addInput(input)
+                if let output = imageOutput {
+                    if session.canAddOutput(output) {
+                        session.addOutput(output)
+                        if let _ = videoConnection {
+                            session.startRunning()
+                            playerLayer = AVCaptureVideoPreviewLayer(session: session)
+                            playerLayer?.frame = viewMain.bounds
+                            viewMain.layer.insertSublayer(playerLayer!, atIndex:0)
+                        }
+                    }
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
